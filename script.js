962577de9647899d110995e51d452e257c36f208
HoSeDo <168710630+HoSeDo@users.noreply.github.com>
2025-05-08 18:23:58 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+mapOption = {
+    center: new kakao.maps.LatLng(37.56808, 126.98046), // 지도의 중심좌표
+    level: 5, // 지도의 확대 레벨
+    mapTypeId : kakao.maps.MapTypeId.ROADMAP // 지도종류
+}; 
+
+// 지도를 생성한다 
+var map = new kakao.maps.Map(mapContainer, mapOption);
+
+// 지도에 확대 축소 컨트롤을 생성한다
+var zoomControl = new kakao.maps.ZoomControl();
+
+// 지도의 우측에 확대 축소 컨트롤을 추가한다
+map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+
+// 지도에 마커를 생성하고 표시한다
+var marker = new kakao.maps.Marker({
+  position: new kakao.maps.LatLng(37.56653, 126.98109), // 마커의 좌표
+   map: map // 마커를 표시할 지도 객체
+});
